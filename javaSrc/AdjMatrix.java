77caf6b2415d58ff8c06062c07ecb2a73bc7abad
MatthewAtkin <s3603797@student.rmit.edu.au>
2018-04-04 13:25:03 +1000

    Update AdjMatrix.java

--- javaSrc/AdjMatrix.java
+++ javaSrc/AdjMatrix.java
@@ -11,56 +11,138 @@ import java.util.*;
  */
 public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
 {
+	ArrayList<ArrayList<Boolean>> matrix;
+	ArrayList<T> vertices;
 
 	/**
 	 * Contructs empty graph.
 	 */
     public AdjMatrix() {
+		matrix = new ArrayList<ArrayList<Boolean>>();
+		vertices = new ArrayList<T>();
     	// Implement me!!
     } // end of AdjMatrix()
     
     
     public void addVertex(T vertLabel) {
-        // Implement me!
+		if(vertices.indexOf(vertLabel) != -1){
+			return; //yeah yeah	
+		}
+			
+		matrix.add(new ArrayList<Boolean>());
+
+		for(int i = 0; i < vertices.size(); i++){
+			matrix.get(i).add(false);
+		}
+
+		for(int i = 0; i <= vertices.size(); i++){
+			matrix.get(vertices.size()).add(false);
+		}
+	
+		vertices.add(vertLabel);
+
+		 // Implement me!
     } // end of addVertex()
 	
     
     public void addEdge(T srcLabel, T tarLabel) {
+		if(vertices.indexOf(srcLabel) == -1){
+			System.err.println(srcLabel + " does not exist");
+			return;		
+		}
+
+		if(vertices.indexOf(tarLabel) == -1){
+			System.err.println(tarLabel + " does not exist");
+			return;		
+		}
+		
+		matrix.get(vertices.indexOf(srcLabel)).set(vertices.indexOf(tarLabel), true);		
+		matrix.get(vertices.indexOf(tarLabel)).set(vertices.indexOf(srcLabel), true);
+		
         // Implement me!
     } // end of addEdge()
 	
 
     public ArrayList<T> neighbours(T vertLabel) {
         ArrayList<T> neighbours = new ArrayList<T>();
-        
-        // Implement me!
+		
+		if(vertices.indexOf(vertLabel) == -1){
+			System.err.println(vertLabel + " does not exist");
+			return neighbours;		
+		}
+
+		for(int i = 0; i < vertices.size(); i++){
+			if(matrix.get(vertices.indexOf(vertLabel)).get(i)){
+				neighbours.add(vertices.get(i));
+			}        
+        }
+		// Implement me!
         
         return neighbours;
     } // end of neighbours()
     
     
     public void removeVertex(T vertLabel) {
-        // Implement me!
+		if(vertices.indexOf(vertLabel) == -1){
+			System.err.println(vertLabel + "does not exist");
+			return;
+		}
+		
+		for(int i = 0; i < vertices.size(); i++){
+			matrix.get(i).remove(vertices.indexOf(vertLabel));
+		}
+
+		matrix.remove(vertices.indexOf(vertLabel));
+
+		vertices.remove(vertLabel);
+		 // Implement me!
     } // end of removeVertex()
 	
     
     public void removeEdge(T srcLabel, T tarLabel) {
-        // Implement me!
+   
+		if(vertices.indexOf(srcLabel) == -1){
+			System.err.println(srcLabel + " does not exist");
+			return;		
+		}
+
+		if(vertices.indexOf(tarLabel) == -1){
+			System.err.println(tarLabel + " does not exist");
+			return;		
+		}
+		
+		matrix.get(vertices.indexOf(srcLabel)).set(vertices.indexOf(tarLabel), false);		
+		matrix.get(vertices.indexOf(tarLabel)).set(vertices.indexOf(srcLabel), false);
+     // Implement me!
     } // end of removeEdges()
 	
     
     public void printVertices(PrintWriter os) {
-        // Implement me!
+        
+        for(int i = 0; i < vertices.size(); i++)
+        {
+        	os.print(vertices.get(i) + " ");
+        }
+
+		os.println();
     } // end of printVertices()
 	
     
     public void printEdges(PrintWriter os) {
-        // Implement me!
+		for(int i = 0; i < vertices.size(); i++){
+			for(int j = 0; j < vertices.size(); j++){
+				if(matrix.get(i).get(j)){
+					os.println(vertices.get(i) + " " + vertices.get(j));
+				}
+			}
+		}
+		 // Implement me!
     } // end of printEdges()
     
     
     public int shortestPathDistance(T vertLabel1, T vertLabel2) {
-    			boolean[] visited = new boolean[vertices.size()];
+    	// Implement me!
+		boolean[] visited = new boolean[vertices.size()];
 		Queue<T> vertQueue = new LinkedList<T>();
 		Queue<Integer> distQueue = new LinkedList<Integer>();
 		T vertex;
@@ -91,8 +173,12 @@ public class AdjMatrix <T extends Object> implements FriendshipGraph<T>
 					}			
 				}
 			}
-		}
+		}	
+		
+    // if we reach this point, source and target are disconnected
         return disconnectedDist;    	
     } // end of shortestPathDistance()
     
 } // end of class AdjMatrix
+About
+Press h to open a hovercard with more details.
